dc4a717aa5b5c2f48bd9eb17033d476a948e6540
hamin <hamin@bisonai.com>
2023-11-15 22:07:46 +0900

    fix: Temp baobab api host

--- apps/finder/src/variants/finder/config/prod/baobab.ts
+++ apps/finder/src/variants/finder/config/prod/baobab.ts
@@ -2,7 +2,7 @@ import { JsonConfigSetting } from '../../../config'
 import { prodNetworks } from './networks'
 
 const config: JsonConfigSetting = {
-    beHost: 'baobab-api.klaytnfinder.io',
+    beHost: 'baobab-api.klaytnfinder.net',
     publicEn: 'https://public-en.klaytnfinder.io/v1/baobab',
     networks: prodNetworks,
 }
