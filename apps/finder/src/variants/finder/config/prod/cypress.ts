043a44a701d3b58a5fad4366288d3760b4739a25
hamin <hamin@bisonai.com>
2023-11-28 17:53:39 +0900

    fix: Update BE host URL in prod config

--- apps/finder/src/variants/finder/config/prod/cypress.ts
+++ apps/finder/src/variants/finder/config/prod/cypress.ts
@@ -2,7 +2,7 @@ import { JsonConfigSetting } from '../../../config'
 import { prodNetworks } from './networks'
 
 const config: JsonConfigSetting = {
-    beHost: 'cypress-api.klaytnfinder.io',
+    beHost: 'cypress-api.klaytnfinder.net',
     publicEn: 'https://public-en.klaytnfinder.io/v1/cypress',
     networks: prodNetworks,
 }
