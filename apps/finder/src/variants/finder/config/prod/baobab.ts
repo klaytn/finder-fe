98a327a80b7de2fd9e66648eac9f32393f8f68bb
hamin <hamin@bisonai.com>
2023-12-07 23:12:35 +0900

    fix: Update API host URLs in prod config

--- apps/finder/src/variants/finder/config/prod/baobab.ts
+++ apps/finder/src/variants/finder/config/prod/baobab.ts
@@ -2,7 +2,7 @@ import { JsonConfigSetting } from '../../../config'
 import { prodNetworks } from './networks'
 
 const config: JsonConfigSetting = {
-    beHost: 'baobab-api.klaytnfinder.net',
+    beHost: 'baobab-api.klaytnfinder.io',
     publicEn: 'https://public-en.klaytnfinder.io/v1/baobab',
     networks: prodNetworks,
 }
--- apps/finder/src/variants/finder/config/prod/cypress.ts
+++ apps/finder/src/variants/finder/config/prod/cypress.ts
@@ -2,7 +2,7 @@ import { JsonConfigSetting } from '../../../config'
 import { prodNetworks } from './networks'
 
 const config: JsonConfigSetting = {
-    beHost: 'cypress-api.klaytnfinder.net',
+    beHost: 'cypress-api.klaytnfinder.io',
     publicEn: 'https://public-en.klaytnfinder.io/v1/cypress',
     networks: prodNetworks,
 }
